4507b1f85b4fa537f67afaad2a539e75ff33da1c
18juba <jubalucas05@gmail.com>
2026-08-20 12:43:04 -0300

    Corrigindo dados recentes

--- src/lib/data/analysisData.ts
+++ src/lib/data/analysisData.ts
@@ -1,2 +1,2 @@
-export const ANALYSIS_JSON_PATH = '/data/fortaleza_analysis_19-08-2026.json';
-export const ANALYSIS_JSON_FILENAME = 'fortaleza_analysis_19-08-2026.json';
+export const ANALYSIS_JSON_PATH = '/data/fortaleza_analysis_20-08-2026.json';
+export const ANALYSIS_JSON_FILENAME = 'fortaleza_analysis_20-08-2026.json';
